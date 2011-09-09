ca1a34dff0c2ed3d14feb327976a9b2d1f705bd7
Jon Anthony <jsa.aerial@gmail.com>
2011-09-09 14:27:30 -0400

    Include ability to interop with C via JNA

--- project.clj
+++ project.clj
@@ -3,36 +3,39 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clojure-csv "1.3.0"]
-		 [clj-shell "0.1.0"]
-		 
+                 [clj-shell "0.1.0"]
+
+                 ;;[clj-native "0.9.1-SNAPSHOT"]
+                 [org.clojars.nathell/clojure-jna "1.0.0-SNAPSHOT"]
+
                  [ring "0.3.5"]
                  [compojure "0.6.0-RC4"]
-		 [aleph, "0.2.0-beta1"]
-		 
-		 [org.bituf/clj-dbcp "0.2"]
-		 [c3p0/c3p0 "0.9.1.2"]
-		 [mysql/mysql-connector-java "5.1.15"]
+                 [aleph, "0.2.0-beta1"]
+
+                 [org.bituf/clj-dbcp "0.2"]
+                 [c3p0/c3p0 "0.9.1.2"]
+                 [mysql/mysql-connector-java "5.1.15"]
                  [commons-net "2.2"]
 
-		 [org.clojure.contrib/macro-utils "1.3.0-alpha4"]
-		 [core.logic "0.6.1-SNAPSHOT"]
-		 
+                 [org.clojure.contrib/macro-utils "1.3.0-alpha4"]
+                 [core.logic "0.6.1-SNAPSHOT"]
+
                  [swingrepl "1.0.0-SNAPSHOT"]
-		 [incanter/incanter-core "1.2.3"]
-		 [incanter/incanter-io "1.2.3"]
-		 [incanter/incanter-charts "1.2.3"]
-		 [incanter/incanter-processing "1.2.3"]
-		 [incanter/incanter-mongodb "1.2.3"]
-		 [incanter/incanter-pdf "1.2.3"]
-		 [incanter/incanter-latex "1.2.3"]
-		 [incanter/incanter-excel "1.2.3"]
+                 [incanter/incanter-core "1.2.3"]
+                 [incanter/incanter-io "1.2.3"]
+                 [incanter/incanter-charts "1.2.3"]
+                 [incanter/incanter-processing "1.2.3"]
+                 [incanter/incanter-mongodb "1.2.3"]
+                 [incanter/incanter-pdf "1.2.3"]
+                 [incanter/incanter-latex "1.2.3"]
+                 [incanter/incanter-excel "1.2.3"]
                  ]
 
   :dev-dependencies [[swank-clojure "1.3.2"]
-		     [lein-clojars "0.6.0"]
-		     [jline "0.9.94"]
-		     ;;[mycroft/mycroft "0.0.2"]
+                     [lein-clojars "0.6.0"]
+                     [jline "0.9.94"]
+                     ;;[mycroft/mycroft "0.0.2"]
                      ]
-  
+
   :repositories {"commons-releases" "http://bizdirusa.com/mirrors/apache"
                  "c3p0-releases"    "http://mirrors.ibiblio.org/pub/mirrors/maven2"})
---
+++ src/edu/bc/jna_utils.clj
@@ -0,0 +1,118 @@
+(ns edu.bc.jna-utils
+
+  "Interfacing with native C lib stuff.  This includes all manner of
+   things from stuff like stdlib, statvfs, string lib, to rnafold native,
+   libsvm, etc.
+   *** CURRENTLY JUST PROOF OF CONCEPT AND EXAMPLES ***"
+
+  (:require [clojure.contrib.math :as math]
+            [clojure.contrib.string :as str]
+            [clojure.contrib.str-utils :as stru]
+            [clojure.set :as set]
+            [clojure.contrib.seq :as seq]
+            [clojure.contrib.io :as io]
+            [clojure.contrib.properties :as prop]
+            [edu.bc.fs :as fs])
+
+  (:use [clojure.contrib.condition
+         :only [raise handler-case *condition*
+                print-stack-trace stack-trace-info]]
+
+        [clojure.contrib.pprint
+         :only (cl-format compile-format)]
+
+        net.n01se.clojure-jna)) ; The key piece
+
+
+(defn jna-malloc
+  "Create a 'C' level USB buffer of size SIZE.  Returns a pair (as a
+   vector): [ptr-to-the-buffer the-buffer] Where ptr-to-the-buffer is
+   a JNA/C ptr object and the-buffer is a java.nio.DirectByteBuffer
+   object." [size]
+  (let [buf (make-cbuf size)
+        ptr (pointer buf)]
+    [ptr buf]))
+
+
+(jna-invoke Integer c/printf "Hello there, %s\n" "Clojure")
+
+(let [[ptr buf] (jna-malloc 88)]
+  (jna-invoke Integer c/statvfs "/data2" ptr)
+  ;; Be wary/careful of machine word size!!!
+  (let [fbsize (.getLong buf)
+        frsize (.getLong buf 8)
+        blocks (.getInt buf 16)
+        bfree  (.getInt buf 24)
+        bavail (.getInt buf 32)
+        files  (.getInt buf 40)
+        ffree  (.getInt buf 48)
+        favail (.getInt buf 56)]
+
+
+    (println "f_fbsize" fbsize)
+    (println "f_frsize" frsize)
+    (println "blocks" (* 4 blocks))
+    (println "bfree" (* 4 bfree))
+    (println "bavail" (* 4 bavail))
+    (println "files" (* 4 files))
+    (println "ffree" (* 4 ffree))
+    (println "favail" (* 4 favail))
+    ))
+
+;;; Let's call RNAfold directly!  Must build a sharable lib from the
+;;; Vienna .os first and place in library path.  Here it is libRNA.so
+;;; Watch out for version 2.0 - it is inconsistent (undefined symbols
+;;; referenced!), but you can hack it to work.
+;;;
+(let [stg "CGCAGGGAUACCCGCG"
+      [ptr buf] (jna-malloc (inc (count stg)))
+      e (jna-invoke Float RNA/fold stg ptr)]
+  (println "E =" e)
+  (println "Structure =" (.getString ptr 0 false)))
+
+(jna-invoke Float RNA/energy_of_struct "CGCAGGGAUACCCGCG" "(((.((....)).)))")
+
+
+
+;;; Example 'arbitrary' C func.
+;;;
+;;; void foo(int *nin, double *x)
+;;; {
+;;;     int n = nin[0];
+;;;
+;;;     int i;
+;;;
+;;;     for (i=0; i<n; i++)
+;;;             x[i] = x[i] * x[i];
+;;; }
+;;;
+;;; gcc -c -fPIC foo.c -o foo.o
+;;; gcc -shared -Wl,-soname,libRTest.so -o libRTest.so  foo.o
+;;;
+;;; Everything seems to work off C allocated "array" things (makes
+;;; sense since that is what C does)
+;;;
+(let [[faptr fabuf] (jna-malloc (* 8 6))
+      [iaptr iabuf] (jna-malloc (* 4 2))
+      fa (double-array [1.0 2.0 3.0 4.0 5.0])]
+
+  ;; Put the Doubles into the "array"
+  (doseq [i (take 5 (iterate inc 0))]
+    (let [f (aget fa i)]
+      (println "f =" f "i =" i)
+      (.setDouble faptr (* 8 i) f)))
+
+  ;; Set the integer count of array "size"
+  (.setInt iaptr 0 5)
+
+  ;; Are they in the "arrays" OK??
+  (doseq [i [0 1 2 3 4]]
+    (println (.getDouble fabuf (* 8 i))))
+  (println (.getInt iabuf))
+
+  ;; Call foo and print return
+  (println (jna-invoke Void RTest/foo iaptr faptr))
+
+  ;; Check to see if results are there.
+  (doseq [i [0 1 2 3 4]]
+    (println (.getDouble fabuf (* 8 i)))))

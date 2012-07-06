c9e176602a3970395299fad3c738cd181de3914c
Jon Anthony <jsa.aerial@gmail.com>
2012-07-06 19:39:55 -0400

    Snapshot: Extending/refactoring, Kind of in pieces continuation of various remote jobs support.

--- resources/gaisr.rb
+++ resources/gaisr.rb
@@ -3,10 +3,18 @@
 require 'rubygems'
 require 'rest_client'
 require 'json'
+require 'fileutils'
+require 'tmpdir'
 
 #ARGV.each do|x|
 #  puts "Argument: #{x}"
 #end
+
+@gaisrDir = File.expand_path("~/.gaisr")
+
+if (!File.directory?(@gaisrDir))
+  Dir.mkdir(@gaisrDir, 775)
+end
 
 if (ARGV.length() == 0)
   puts "Use -h or --help for usage"
@@ -16,10 +24,10 @@ end
 sub_cmds = ["name-taxonomy",
             "sto-csv-matchup"]
 
-cmd= ARGV[0]
+@cmd= ARGV[0]
 args = ARGV[1,ARGV.length]
 
-if (cmd == "-h" or cmd == "--help")
+if (@cmd == "-h" or @cmd == "--help")
   puts "Usage:\n"
   puts "gaisr subcmd args"
   puts ""
@@ -33,13 +41,13 @@ if (cmd == "-h" or cmd == "--help")
   exit(0)
 end
 
-if (sub_cmds.include?(cmd))
+if (sub_cmds.include?(@cmd))
   argstr = args.join(" ")
-  puts `#{cmd} #{argstr}` # execute cmd, results go to term
+  puts `#{@cmd} #{argstr}` # execute cmd, results go to term
   exit(0)
 elsif (not ['check-sto', 'correct-sto-coordinates',
-            'run-config', 'check-job'].include?(cmd))
-  puts "Unknown subcmd #{cmd}"
+            'run-config', 'check-job'].include?(@cmd))
+  puts "Unknown subcmd #{@cmd}"
   puts "use gaisr -h, for useage"
   exit(1)
 end
@@ -61,39 +69,140 @@ def remote_cmd (upload_type, infile)
                          {:cookies => {:user => ENV['LOGNAME'].downcase}})
 end
 
+
 def report_result (header, result)
-  result = JSON.parse(result.body)
+  result = if result.is_a?(String) then result else JSON.parse(result.body) end
   if (result["stat"] != "success")
     puts "Error - #{result['stat']}"
   else
     puts ""
-    puts header
+    if (header != "")
+      puts header
+    end
     puts result["info"]
   end
+  result
 end
 
 
-if (cmd == "check-sto")
-  args.each do |infile|
-    ##infile = File.expand_path(args[0])
-    result = remote_cmd(cmd, infile)
-    report_result(infile, result)
-  end
-elsif (cmd == "check-job")
-  jobid = args[0]
-  jobidfile = "job"+jobid
+def create_jobid_file (jobid, content)
+  jobidfile = @gaisrDir + "/job"+jobid
   jobidFile = File.new(jobidfile, "w")
   jobidFile.syswrite("#{jobid}\n")
+  if (!content.emtpy?)
+    jobidFile.syswrite(content.join("\n"))
+  end
   jobidFile.close
-  result = remote_cmd(cmd, jobidfile)
-  report_result("Status :", result)
+  return jobidfile
+end
+
+
+def correct_sto_finish (basedir, info)
+  ## it's amazing how verbose and convoluted this all is.  You
+  ## could easily do this entire function in three lines of Clojure.
+  ## Ruby sucks!!!
+  info = result["info"]
+  filename = info.first
+  contents = info[1..info.length]
+  suffixes = ["-new.sto", "-diffs.txt", "-bad.txt"]
+  names = suffixes.map do |x| "my-sto.sto".sub(/\.sto/, x) end
+  both = [names, contents]
+  both.transpose.each do |fname, lines|
+      if (!lines.empty?)
+        outFile = File.new(basedir+fname, w)
+        if outFile
+          outFile.syswrite(lines)
+          outFile.close
+        else
+          puts "Unable to open file #{fname}"
+        end
+      end
+    end
+end
+
+
+def check_finish (result)
+  
+  File.dirname()
+
+
+
+
+
+def check_job (args)
+  if (args == [])
+    jobidfile = File.join(@gaisrDir, "last-job.txt")
+    if (!File.exists?(jobidfile))
+      puts "Error - you have no jobs"
+      exit(1)
+    end
+  else
+    jobid = args[0]
+    jobidfile = "job"+jobid
+    jobidfile = File.join(@gaisrDir, jobidfile)
+  end
+  if File.exists?(jobidfile)
+    data = IO.readlines(jobidfile).map do |x| x.chomp end
+    jobid = data[0]
+    tempfile = File.join(Dir.tmpdir, jobid)
+    tempFile = File.new(tempfile, "w")
+    tempFile.puts(jobid)
+    tempFile.close
+    result = remote_cmd(@cmd, tempfile)
+    File.delete(tempfile)
+    result = report_result("Status:", result)
+  else
+    puts "No job with id #{jobid} found"
+  end
+end
+
+  jobidfile = File.join(@gaisrDir, "job"+jobid)
+  basedir = IO.readlines
+
+  result = JSON.parse(result.body)
+  if (result["stat"] != "success")
+    puts "Error - #{result['stat']}"
+  else
+
+
+def correct_sto (args)
+  args.each do |infile|
+    infile = File.expand_path(infile)
+    result = remote_cmd(@cmd, infile)
+    jobid = result["info"][1]
+    jobidfile = create_jobid_file(jobid, [@cmd, infile])
+    report_result("" result)
+end
+
+
+def run_job (config_file)
+  result = remote_cmd(@cmd, config_file)
+  result = report_result("", result)
+  jobid = result["info"][1]
+  jobidfile = create_jobid_file(jobid, [@cmd, config_file]
+  FileUtils.cp(jobidfile, @gaisrDir + "/last-job.txt")
+end
+
+
+if (@cmd == "check-sto")
+  args.each do |infile|
+    infile = File.expand_path(infile)
+    result = remote_cmd(@cmd, infile)
+    report_result(infile, result)
+  end
+
+elsif (@cmd == "correct-sto-coordinates")
+  coorect_sto(args)
+
+elsif (@cmd == "check-job")
+  check_job(args)
+
 elsif (args.length > 1)
-  puts "#{cmd} has only single config file parameter, not #{args.length}"
+  puts "#{@cmd} has only single config file parameter, not #{args.length}"
   exit(1)
 else
   config_file = File.expand_path(args[0])
-  result = remote_cmd(cmd, config_file)
-  report_result("", result)
+  run_job(config_file)
 end
 
 
